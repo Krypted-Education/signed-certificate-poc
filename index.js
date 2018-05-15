1d814331538c66c05b4d8d2eb1dc7a5de345e795
msusur <mail@mertsusur.com>
2018-05-15 22:23:27 +0100

    Add npm flow and fix indent

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express'),
-fs = require('fs'),
-formidable = require('formidable'),
-app = new express();
+  fs = require('fs'),
+  formidable = require('formidable'),
+  app = new express();
 app.set('port', process.env.PORT || 5000);
+
 app.listen(app.get('port'), () => {
-console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
