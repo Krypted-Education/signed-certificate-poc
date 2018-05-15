de0af2995ee1de26e53dcb2735af73e39308a108
msusur <mail@mertsusur.com>
2018-05-15 22:31:33 +0100

    Adds static pages

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express'),
   fs = require('fs'),
   formidable = require('formidable'),
   app = new express();
+
 app.set('port', process.env.PORT || 5000);
+
+app.use("/", express.static("static"));
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
