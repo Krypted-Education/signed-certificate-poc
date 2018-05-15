b0ca117e72ff0b5f421477df05b904753cde0b6d
unknown <sanane1212>
2018-05-16 01:51:21 +0300

    to json

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express'),
-  fs = require('fs'),
-  formidable = require('formidable'),
-  app = new express();
-
+const express = require('express');
+var bodyParser = require('body-parser');
+fs = require('fs'),
+formidable = require('formidable'),
+app = new express(); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
 
 app.use("/", express.static("static"));
-
+app.post("/uploadfile", function (req, res) {
+  console.log(req.body.individualName);
+  console.log(req.body.individualLastName);
+  console.log(req.body.fileInput);
+});
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
