65b433480591f8ce4707670901772a4568bd93da
unknown <sanane1212>
2018-05-16 18:32:19 +0300

    changed wrong codes

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-fs = require('fs'),
-formidable = require('formidable'),
+  const express = require('express');
+  var bodyParser = require('body-parser');
+  fs = require('fs'),
+  formidable = require('formidable'),
 app = new express(); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
 
-app.use("/", express.static("static"));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-app.post('/',function(req,res){
-  var user_name=req.body.user;
-  console.log("User name = "+user_name);
-  res.end("yes");
+app.use('/', express.static('static'));
+app.get('/', (req, res) => res.send(__dirname + '/index.html'));
+app.post('/', (req, res) => {
+  let user_name = req.body.user;
+  console.log('User name = ' + user_name);
+  res.end('yes');
 });
 
 app.listen(app.get('port'), () => {
