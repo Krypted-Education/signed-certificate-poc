ae955ee3815ede71aa91929a92872be1bac93fd3
unknown <sanane1212>
2018-05-18 16:54:41 +0300

    File upload to Ethereum Swarm

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const fs = require('fs');
-  const formidable = require('formidable');
-  const multer  = require('multer');
-  var upload = multer();
-  var Bzz = require('web3-bzz');
-  var bzz = new Bzz('http://localhost:8500');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  fs = require('fs'),
+  formidable = require('formidable'),
+  multer = require('multer'),
+  upload = multer(),
+  Bzz = require('web3-bzz'),
+  bzz = new Bzz('http://localhost:8500'),
+  app = new express();
+
 // change provider
 bzz.setProvider('http://swarm-gateways.net');
-app = new express(); 
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.set('port', process.env.PORT || 5000);
 
 app.use('/', express.static('static'));
-app.get('/', (req, res) => res.send(__dirname + '/index.html'));
 app.post('/fileupload', upload.single('thumbnail'), (req, res, next) => {
   const signatureFile = {
     name: req.body.name,
     surname: req.body.surname,
     file: req.file,
   };
-  
+
   const dir = {
     "/signatureFile.txt": {
       type: "application/json",
@@ -35,6 +37,7 @@ app.post('/fileupload', upload.single('thumbnail'), (req, res, next) => {
     })
     .catch(err => res.status(500).json({ error: err, file: signatureFile }));
 });
+
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
