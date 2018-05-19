a0b3f78a17842129dbfeddbf3e62c7868d2788fc
unknown <sanane1212>
2018-05-19 14:55:54 +0300

    Hashing json object

--- services/signature-service.js
+++ services/signature-service.js
@@ -1,8 +1,9 @@
+  sha256 = require('sha256');
 const sign = object => {
   const json = JSON.stringify(object);
-
+  var hashing = sha256(json);
   // TODO: sign the json string and return the signature here.
-
+  console.log(hashing);
   return json;
 };
 
