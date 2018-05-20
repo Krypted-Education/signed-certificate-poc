1110145da8ec7ca1f11ae6b115d37e9684a14a2d
msusur <mail@mertsusur.com>
2018-05-20 11:39:26 +0100

    reset form state before saving

--- static/lib/app.js
+++ static/lib/app.js
@@ -15,11 +15,14 @@
       },
       save: function(event) {
         this.hash = false;
+        this.isLoading = true;
+        this.isError = false;
+
         var form = {
           name: this.name,
           lastname: this.lastname
         };
-        this.isLoading = true;
+
         this.$http
           .post('/api/create/certificate', form)
           .then(function(response) {
@@ -34,7 +37,7 @@
             }
           }, function(err) {
             this.isLoading = false;
-            this.error(error);
+            this.error(err);
           });
       }
     }
