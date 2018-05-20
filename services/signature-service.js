721d51693390ffc552c2b15ba347f57216e8b779
msusur <mail@mertsusur.com>
2018-05-20 11:11:23 +0100

    Add the sha256 signature

--- services/signature-service.js
+++ services/signature-service.js
@@ -1,9 +1,10 @@
+const crypto = require('crypto'),
+  // This secret should move to an environment variable. Keeping the secret on the source code isn't secure at all.
+  secret = 'u#*_wNN3ULKAsaYqFKxezg=-QgX&V8HPk@rgDVDv=Nu37T2+@$=9^^p$8QWj*ZPtr6%GC?EWmZy^QDmtuv8j_S3+bd2AW*tWwYZ#-#%+PW4h=!AxkGvfYxZKtzXvLQGV';
+
 const sign = object => {
   const json = JSON.stringify(object);
-
-  // TODO: sign the json string and return the signature here.
-
-  return json;
+  return crypto.createHmac('sha256', secret).update(json).digest('base64');
 };
 
 module.exports = {
