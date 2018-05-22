80ccf191a80feeaf5a07c2eca37548133fbc2b5f
Mert Susur <mail@mertsusur.com>
2018-05-22 13:59:36 +0100

    Adds signature validation for ethereum address

--- services/signature-service.js
+++ services/signature-service.js
@@ -1,11 +1,15 @@
 const crypto = require('crypto'),
-  EthCrypto = require('eth-crypto'),
+  Web3 = require('web3'),
   // This secret should move to an environment variable. Keeping the secret on the source code isn't secure at all.
-  secret = 'u#*_wNN3ULKAsaYqFKxezg=-QgX&V8HPk@rgDVDv=Nu37T2+@$=9^^p$8QWj*ZPtr6%GC?EWmZy^QDmtuv8j_S3+bd2AW*tWwYZ#-#%+PW4h=!AxkGvfYxZKtzXvLQGV';
+  secret =
+    'u#*_wNN3ULKAsaYqFKxezg=-QgX&V8HPk@rgDVDv=Nu37T2+@$=9^^p$8QWj*ZPtr6%GC?EWmZy^QDmtuv8j_S3+bd2AW*tWwYZ#-#%+PW4h=!AxkGvfYxZKtzXvLQGV';
 
 const sign = object => {
   const json = JSON.stringify(object);
-  return crypto.createHmac('sha256', secret).update(json).digest('base64');
+  return crypto
+    .createHmac('sha256', secret)
+    .update(json)
+    .digest('base64');
 };
 
 const validateKryptedSignature = (signature, object) => {
@@ -15,13 +19,12 @@ const validateKryptedSignature = (signature, object) => {
 };
 
 const validateEthereumSignature = (signature, address, object) => {
-  const signatureMessage = `I, ${object.profile.issuerAuthority}, signing this diploma as the issuer`;
-
-  const signer = EthCrypto.recoverPublicKey(
-    signature,
-    EthCrypto.hash.keccak256(signatureMessage)
-  );
-  return signer === address;
+  const signatureMessage = `I, ${
+    object.profile.issuerAuthority
+  }, signing this diploma as the issuer`;
+  var web3 = new Web3(new Web3.providers.HttpProvider(''));
+  var signer = web3.eth.accounts.recover(signatureMessage, signature);
+  return signer.toUpperCase() === address.toUpperCase();
 };
 
 module.exports = {
